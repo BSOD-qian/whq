1cabf9364ec3a757e3d1271adad4e928f7d05d5d
bbBSODdd <3348640434@qq.com>
2024-09-05 21:26:11 +0800

    20240905

---
+++ name.js
@@ -0,0 +1,6 @@
+for (i = 0; i < 999; i++) {
+    document.getElementsByClassName('no09')[i].innerHTML = "daydream"
+    document.getElementsByClassName('no19')[i].innerHTML = "都怨你"
+    document.getElementsByClassName('no23')[i].innerHTML = "￴L.￴"
+    document.getElementsByClassName('no26')[i].innerHTML = "IRieNnE"
+}
