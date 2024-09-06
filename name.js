bd2389a4b33b8936f377e8f712e3a8bfc689bca3
bbBSODdd <3348640434@qq.com>
2024-09-06 19:35:55 +0800

    20240906

--- name.js
+++ name.js
@@ -1,4 +1,5 @@
 document.querySelectorAll(".no09").forEach(function (content) { content.textContent = "daydream" })
 document.querySelectorAll(".no19").forEach(function (content) { content.textContent = "都怨你" })
 document.querySelectorAll(".no23").forEach(function (content) { content.textContent = "L." })
+document.querySelectorAll(".no25").forEach(function (content) { content.textContent = "￴￴ ￴￴ ￴" })
 document.querySelectorAll(".no26").forEach(function (content) { content.textContent = "IRieNnE" })
